ccb0dfae2d9720a1d4551c9387850d487b56ca13
asdfish <lgray3420@gmail.com>
2024-11-26 09:48:47 -0500

    added cpp compatability

--- carp.h
+++ carp.h
@@ -15,25 +15,54 @@ enum CarpArgumentType {
   KEY,
 };
 
+#ifdef __cplusplus
 typedef int (*carp_callback) (char flag_short, enum CarpArgumentType argument_type, const char* argument, void* user_data);
+#else
+typedef int (*carp_callback) (char flag_short, enum CarpArgumentType argument_type, const char* restrict argument, void* restrict user_data);
+#endif
 
+#ifdef __cplusplus
+extern int carp_parse(int argc, const char** argv, const struct CarpOption* options, size_t options_length, carp_callback callback, void* user_data);
+#else
 extern int carp_parse(int argc, const char** restrict argv, const struct CarpOption* restrict options, size_t options_length, carp_callback callback, void* user_data);
+#endif
 
 #ifdef CARP_IMPLEMENTATION
+#ifdef __cplusplus
+#include <cstring>
+#else
+#include <stdbool.h>
 #include <string.h>
+#endif
+
 #include <sys/types.h>
 
-int carp_parse(int argc, const char** restrict argv, const struct CarpOption* restrict options, size_t options_length, carp_callback callback, void* user_data) {
+bool passed_double_dash = false;
+
+#ifdef __cplusplus
+int carp_parse(int argc, const char** argv, const struct CarpOption* options, size_t options_length, carp_callback callback, void* user_data)
+#else
+int carp_parse(int argc, const char** restrict argv, const struct CarpOption* restrict options, size_t options_length, carp_callback callback, void* user_data)
+#endif
+{
   int result = -1;
 
   for(int i = 1; i < argc; i ++) {
-    if(argv[i][0] != '-' || strlen(argv[i]) <= 1)
-      goto callback_default;
+    if(argv[i][0] != '-' || strlen(argv[i]) <= 1) {
+      result = callback(0, KEY, argv[i], user_data);
+      if(result != 0)
+        return result;
+    }
 
-    bool long_flag = strncmp(argv[i], "--", 2) == 0;
+    bool long_flag = strncmp(argv[i], "--", 2) == 0 && !passed_double_dash;
 
     ssize_t option_index = -1;
     if(long_flag) {
+      if(strcmp(argv[i], "--") == 0) {
+        passed_double_dash = true;
+        continue;
+      }
+
       char* flag = (char*) argv[i] + 2;
       for(size_t i = 0; i < options_length; i ++)
         if(strcmp(flag, options[i].flag_long) == 0) {
@@ -49,21 +78,16 @@ int carp_parse(int argc, const char** restrict argv, const struct CarpOption* re
         }
     }
 
-    if(option_index == -1)
-      goto callback_default;
-
-    if(options[option_index].require_argument) {
-      result = callback(options[option_index].flag_short, FLAG, i ++ >= argc ? NULL : argv[i], user_data);
-      goto parse_result;
-    } else {
-      result = callback(options[option_index].flag_short, FLAG, NULL, user_data);
-      goto parse_result;
+    if(option_index == -1) {
+      result = callback(0, KEY, argv[i], user_data);
+      if(result != 0)
+        return result;
     }
 
-callback_default:
-    result = callback(0, KEY, argv[i], user_data);
-
-parse_result:
+    if(options[option_index].require_argument)
+      result = callback(options[option_index].flag_short, FLAG, i ++ >= argc ? NULL : argv[i], user_data);
+    else
+      result = callback(options[option_index].flag_short, FLAG, NULL, user_data);
     if(result != 0)
       return result;
   }
